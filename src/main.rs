c7cc2176eda9c59677d2afc3e0d81d362007d158
Marvin Sextro <marvin.sextro@gmail.com>
2021-04-27 21:31:06 +0200

    Implement simple analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env, io, fs::File, process,
+    io::{
+        BufRead, BufReader
+    },
+};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Reading {:?} ...", config.path);
+    analyze_file(File::open(config.path.as_str())?);
+
+    Ok(())
+}
+
+struct Config {
+    path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+
+        let path = args[1].clone();
+        Ok(Config { path })
+    }
+}
+
+fn analyze_file(file: File) {
+    let buf_reader = BufReader::new(file);
+
+    let mut rust_commits: i32 = 0;
+    let mut all_commits: i32 = 0;
+
+    for line in buf_reader.lines() {
+        let line = line.unwrap_or_else(|err| {
+            println!("Problem reading line: {}", err);
+            process::exit(1);
+        });
+
+        if line.to_string().contains(".rs") {
+            rust_commits += 1;
+        }
+
+        all_commits += 1;
+    }
+
+    let percentage: f32 = (rust_commits as f32 / all_commits as f32) * 100.;
+
+    println!("File contains {:?} / {:.2}% commits with rust", rust_commits, percentage);
 }
