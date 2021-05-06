cd8249185b444ee88d719eed317b79987cdbdd07
Marvin Sextro <marvin.sextro@gmail.com>
2021-05-06 22:30:38 +0200

    Count individual repos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     env, io, fs::File, process,
     io::{
         BufRead, BufReader
@@ -35,18 +36,23 @@ impl Config {
 
 fn analyze_file(file: File) {
     let buf_reader = BufReader::new(file);
-
+    
+    let mut repos = HashSet::new();
     let mut rust_commits: i32 = 0;
     let mut all_commits: i32 = 0;
 
-    for line in buf_reader.lines() {
-        let line = line.unwrap_or_else(|err| {
+    for raw_line in buf_reader.lines() {
+        let line = raw_line.unwrap_or_else(|err| {
             println!("Problem reading line: {}", err);
             process::exit(1);
-        });
+        }).to_string();
 
-        if line.to_string().contains(".rs") {
+        if line.contains(".rs") {
             rust_commits += 1;
+            
+            let data_points: Vec<&str> = line.split("\t").collect();
+            let repo = String::from(data_points[0]);
+            repos.insert(repo);
         }
 
         all_commits += 1;
@@ -54,5 +60,6 @@ fn analyze_file(file: File) {
 
     let percentage: f32 = (rust_commits as f32 / all_commits as f32) * 100.;
 
-    println!("File contains {:?} / {:.2}% commits with rust", rust_commits, percentage);
+    println!("File contains {:?} of {:?} or {:.2}% commits with rust", rust_commits, all_commits, percentage);
+    println!("The commits were made in {:?} repos", repos.len());
 }
